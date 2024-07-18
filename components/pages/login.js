6a00fcddd88ddc9aa9898a87f7204b87e85065f2
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-07-18 11:03:58 +0000

    HK

--- components/pages/login.js
+++ components/pages/login.js
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function Login({ onLogin }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://api.lokivpn.com/api/admin/login', { email, password });
+            const response = await axios.post('https://api.lokivpn.com/api/admin/login', { Email, Password });
             const token = response.data.token;
             localStorage.setItem('token', token);
             onLogin(); // Call the onLogin prop to notify the parent component
@@ -20,8 +20,8 @@ function Login({ onLogin }) {
 
     return (
         <form onSubmit={handleLogin}>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+            <input type="email" value={Email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+            <input type="password" value={Password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <button type="submit">Login</button>
         </form>
     );
